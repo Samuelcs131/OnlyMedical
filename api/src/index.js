883b8b2fb851eadb82779214d8a6fce3c6235592
Samuel Claudino <samuelcs131@gmail.com>
2022-03-08 03:19:04 -0300

    remove project

--- api/src/index.js
+++
@@ -1,6 +0,0 @@
-import { ApolloServer } from 'apollo-server'
-import { typeDefs, resolvers } from './grapql/merging'
-
-const SERVER = new ApolloServer({ typeDefs, resolvers })
-
-SERVER.listen().then(({ url }) => { console.log(`🚀 Servidor pronto em ${url}`); });
